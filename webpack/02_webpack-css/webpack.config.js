6450b62b3ca8cdbc312774408d449e3fe70752d8
袁振家 <1914296109@qq.com>
2022-04-18 13:59:01 +0800

    1.webpack

---
+++ webpack/02_webpack-css/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/main.js",
+    output: {
+        path: path.resolve(__dirname,"./build"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test:/\.css$/, //,正则表达式
+                // loader: "css-loader"
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            }
+        ]
+    }
+}
