73cb15911b457080fd1a07429489dcb669154af0
袁振家 <1914296109@qq.com>
2022-05-11 19:29:14 +0800

    ci: vue3 diff算法

--- vue3/mini-vue/01_渲染器实现/renderer.js
+++ vue3/mini-vue/01_渲染器实现/renderer.js
@@ -41,8 +41,9 @@ const mount = (vnode, container) => {
 };
 
 
-// fiff 算法
+// 修改dom的时候，进行 diff 算法判断
 const patch = (n1, n2) => {
+    // 如果标签不同，那么直接去掉之前的vnode节点，添加新的
     if (n1.tag !== n2.tag) {
         const n1ElParent = n1.el.parentElement
         n1ElParent.removeChild(n1.el)
@@ -79,6 +80,43 @@ const patch = (n1, n2) => {
         }
 
         // 3.处理 children
+        const oldChildren = n1.children || {}
+        const newChildren = n2.children || {}
+        if (typeof newChildren === "string") { // 情况一： newChildren 本身是string
+          // 边界情况 (edge case)
+          if (typeof oldChildren === "string") {
+            if (oldChildren !== newChildren) {
+              el.textContent = newChildren
+            }
+          } else {
+            el.innerHTML = newChildren
+          } 
+        } else { // 情况二： newChildren 本身是个数组
+          if (typeof oldChildren === "string") {
+            el.innerHTML = ""
+            newChildren.forEach(item => {
+              mount(item, el)
+            });
+          } else {
+            // 判断新旧子节点的长度，取长度短的去遍历
+            // 2. 如果旧子节点长，多出来的直接去掉
+            const commonLength = Math.min(oldChildren.length, newChildren.length)
+            for (let i = 0;i < commonLength;i++) {
+              patch(oldChildren[i], newChildren[i])
+            }
 
+            // 1. 如果新子节点长，那么多出来的直接添加     
+            if (oldChildren.length < newChildren.length) {
+              newChildren.slice(commonLength).forEach(item => {
+                mount(item, el)
+              })
+            } else {
+              // 2. 如果旧子节点长，多出来的直接去掉
+              oldChildren.slice(commonLength).forEach(item => {
+                el.removeChild(item.el)
+              }) 
+            }
+          }
+        }
     }
 }
