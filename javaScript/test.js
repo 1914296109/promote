053adc34613148811703a613d41ce44e6eec56b0
袁振家 <1914296109@qq.com>
2022-05-12 10:14:33 +0800

    ci: javascript对象的操作

--- javaScript/test.js
+++ javaScript/test.js
@@ -1,14 +1,1 @@
 function fn() {}
-
-let obj = {
-  name: "1",
-  age: 1,
-};
-let test = Object.keys(obj);
-console.log(test);
-
-let demo = document.querySelector(".demo")
-let demoList = document.querySelectorAll(".demo")
-console.log(demo);
-console.log(demoList);
-console.log(Array.prototype.slice.call(demoList,0));
