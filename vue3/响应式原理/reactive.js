25eb1a1c81d1bc71757d241a6d037ab77c09c8ad
袁振家 <1914296109@qq.com>
2022-05-26 09:46:02 +0800

    ci: vue响应式原理

---
+++ vue3/响应式原理/reactive.js
@@ -0,0 +1,81 @@
+class Dep {
+  constructor() {
+    // 创建一个依赖集合
+    this.subscribers = new Set();
+  }
+
+  // 添加依赖
+  depend() {
+    if (activeEffect) {
+      this.subscribers.add(activeEffect);
+    }
+  }
+
+  // 通知
+  notify() {
+    this.subscribers.forEach((effect) => effect());
+  }
+}
+
+let activeEffect = null;
+function watchEffect(Effect) {
+  activeEffect = Effect;
+  //   dep.depend();
+  Effect();
+  activeEffect = null;
+}
+
+// Map({key: value}): key是一个字符串
+// WeakMap({key(对象): value}): key是一个对象，弱引用
+const targetMap = new WeakMap();
+function getDep(target, key) {
+  // 根据对象(target)取出对应的 Map 对象
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    depsMap = new Map();
+    targetMap.set(target, depsMap);
+  }
+
+  // 2. 取出具体的 dep 对象
+  let dep = depsMap.get(key);
+  if (!dep) {
+    dep = new Dep();
+    depsMap.set(key, dep);
+  }
+  return dep;
+}
+
+// vue2 对 raw 进行数据劫持
+function reactive(raw) {
+  Object.keys(raw).forEach((key) => {
+    const dep = getDep(raw, key); 
+    let value = raw[key];
+    Object.defineProperty(raw, key, {
+      get() {
+        dep.depend();
+        return value
+      },
+      set(newValue) {
+        if (newValue !== value) {
+          value = newValue;
+          dep.notify();
+        }
+      },
+    });
+  });
+  return raw;
+}
+
+const info = reactive({ count: 100, age: 18 });
+const foo = reactive({ height: 1.88 });
+
+watchEffect(function () {
+  console.log('1', info.count * 2, info.age);
+});
+
+watchEffect(function () {
+  console.log('2', info.count * info.count);
+});
+
+info.count = 1
+// dep.notify();
